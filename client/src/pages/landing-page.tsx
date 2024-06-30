4e309a880f1c2858a42f64a9c96ff37fcd873898
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-06-30 19:00:15 +0200

    feat: add animations with Framer Motion

--- client/src/pages/landing-page.tsx
+++ client/src/pages/landing-page.tsx
@@ -1,37 +1,78 @@
+import { motion } from "framer-motion";
+
 function LandingPage() {
   return (
     <main className="h-screen w-screen flex bg-slate-100 px-24">
       <div className="flex flex-col gap-y-2.5 justify-center items-start flex-1 h-full">
-        <h3 className="font-bold text-3xl">
-          Recipe<span className="text-primary-orange">Book</span>
-        </h3>
-        <h1 className="font-bold text-7xl">
-          Everyone <br />
-          Can Be a Chef
+        <h1 className="font-bold text-6xl">
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          >
+            Everyone
+          </motion.span>
+          <br />
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.2, delay: 1.5 }}
+          >
+            Can Be a Chef
+          </motion.span>
+          <br />
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.2, delay: 1.5 }}
+          >
+            with Recipe<span className="text-primary-orange">Book</span>
+          </motion.span>
         </h1>
-        <span className="bg-orange-300/40 rounded-md py-1 px-3 text-primary-orange font-medium">
+        <motion.span
+          className="bg-orange-300/40 rounded-md py-1 px-3 text-primary-orange font-medium"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2, delay: 1.5 }}
+        >
           🔥 The largest site for food recipes
-        </span>
-        <p className="text-xl text-secondary pt-6">
+        </motion.span>
+        <motion.p
+          className="text-xl text-secondary pt-6"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2, delay: 1.5 }}
+        >
           Discover our
           <span className="text-primary-orange font-medium">
             {" "}
             1000+ recipes{" "}
           </span>
           and start sharing your own delicious creations with us.
-        </p>
-        <button className="bg-btn-primary hover:bg-btn-primary-hovered w-60 py-3 rounded-md text-white font-bold mt-4">
+        </motion.p>
+        <motion.button
+          className="bg-btn-primary hover:bg-btn-primary-hovered w-60 py-3 rounded-md text-white font-bold mt-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2, delay: 1.5 }}
+        >
           Explore
-        </button>
+        </motion.button>
       </div>
-      <div className="flex-1 flex items-center justify-center">
+      <motion.div
+        className="flex-1 flex items-center justify-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 1.5 }}
+      >
         <img
           src={"/assets/placeholder.png"}
           alt={"Hero image"}
           className="w-full h-auto"
         />
-      </div>
+      </motion.div>
     </main>
   );
 }
+
 export default LandingPage;
