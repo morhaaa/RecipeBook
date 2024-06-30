adcb8145bf2d4fdb8f2c21f9f5a00735306f5a53
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-06-30 18:40:59 +0200

    style: set standard colors and add styles for LandingPage component

--- client/src/pages/landing-page.tsx
+++ client/src/pages/landing-page.tsx
@@ -1,21 +1,34 @@
 function LandingPage() {
   return (
-    <main className="h-screen w-screen flex">
-      <div className="flex-1">
-        <h3>RecipeBook</h3>
-        <h1>Everyone Can Be a Chef</h1>
-        <p>
-          Discover our 1000+ recipes and start sharing your own delicious
-          creations with us.
+    <main className="h-screen w-screen flex bg-slate-100 px-24">
+      <div className="flex flex-col gap-y-2.5 justify-center items-start flex-1 h-full">
+        <h3 className="font-bold text-3xl">
+          Recipe<span className="text-primary-orange">Book</span>
+        </h3>
+        <h1 className="font-bold text-7xl">
+          Everyone <br />
+          Can Be a Chef
+        </h1>
+        <span className="bg-orange-300/40 rounded-md py-1 px-3 text-primary-orange font-medium">
+          🔥 The largest site for food recipes
+        </span>
+        <p className="text-xl text-secondary pt-6">
+          Discover our
+          <span className="text-primary-orange font-medium">
+            {" "}
+            1000+ recipes{" "}
+          </span>
+          and start sharing your own delicious creations with us.
         </p>
-        <button>Explore</button>
+        <button className="bg-btn-primary hover:bg-btn-primary-hovered w-60 py-3 rounded-md text-white font-bold mt-4">
+          Explore
+        </button>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 flex items-center justify-center">
         <img
           src={"/assets/placeholder.png"}
           alt={"Hero image"}
-          height={100}
-          width={100}
+          className="w-full h-auto"
         />
       </div>
     </main>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f172a",
+        secondary: "#374151",
+        "primary-orange": "#ea580c",
+        "btn-primary": "#f97316",
+        "btn-primary-hovered": "#ea580c",
+      },
+    },
   },
   plugins: [],
 };
