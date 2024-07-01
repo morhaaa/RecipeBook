b3802e5ded42ec3a7ffdc85b3eca845c4ac8c903
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-01 15:52:33 +0200

    feat: create button for upload a recipe

--- client/src/pages/recipes.tsx
+++ client/src/pages/recipes.tsx
@@ -8,12 +8,18 @@ function Recipes() {
   return (
     <main className="min-h-screen">
       <Header />
-      <div className="px-24 py-20 flex flex-col gap-y-5">
+      <div className="px-20 py-20 flex flex-col gap-y-5">
         <SearchBar />
-        <Filters
-          filters={["Vegetarian", "Gluten-free", "Mexican", "Healthy"]}
-          activeFilters={["Mexican"]}
-        />
+        <div className="flex justify-between w-full items-center">
+          <Filters
+            filters={["Vegetarian", "Gluten-free", "Mexican", "Healthy"]}
+            activeFilters={["Mexican"]}
+          />
+          <button className="bg-btn-primary hover:bg-btn-primary-hovered px-6 py-1.5 rounded-md text-white font-medium">
+            Upload your recipe
+          </button>
+        </div>
+
         <div className="flex-1 flex flex-wrap justify-center gap-10 pt-4">
           {Array.from(Array(8)).map((_, i) => (
             <CardRecipe
