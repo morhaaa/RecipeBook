32c6ba7ac8ce204a91cb70a897e1f4f2e52978b2
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-02 08:08:49 +0200

    feat: implement search bar logic

--- client/src/components/search-bar.tsx
+++ client/src/components/search-bar.tsx
@@ -1,6 +1,15 @@
 import { Search } from "lucide-react";
 
-function SearchBar() {
+interface SearchBarProps {
+  onSearch: (searchValue: string) => void;
+  value: string;
+}
+
+function SearchBar({ onSearch, value }: SearchBarProps) {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(event.target.value);
+  };
+
   return (
     <div className="flex items-center w-full text-secondary bg-white/70 border border-gray-300 rounded-xl py-1.5 shadow overflow-hidden pl-4">
       <Search />
@@ -8,6 +17,8 @@ function SearchBar() {
         type="text"
         placeholder="Search recipes..."
         className="flex-grow px-4 py-2 outline-none bg-white/70"
+        value={value}
+        onChange={handleInputChange}
       />
     </div>
   );
