47acebe6041c39ab8d3931fbe889583103f133d1
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-02 08:08:15 +0200

    chore: remove unnecessary function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -11,11 +11,3 @@ export function cn(...inputs: ClassValue[]) {
 export function resolvePath(path: string): string {
   return BASE_URL + path;
 }
-
-export function getRandomRating(): string {
-  const random_number = Math.random(); // number between 0 and 1
-  const value = random_number * 3; // value between 3 and 5
-  const rounded_value = Math.floor(value);
-
-  return rounded_value.toString();
-}
