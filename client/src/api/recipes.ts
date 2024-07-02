2ac8e1909ec33e0b76eeff092a9d4b92c53358c5
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-02 08:11:39 +0200

    feat: implement recipes api and recipes page logic

--- client/src/api/recipes.ts
+++ client/src/api/recipes.ts
@@ -1,6 +1,34 @@
+import { resolvePath } from "../lib/utils";
 import { axiosInstance } from "./axios";
 
-export const getRecipes = async () => {
-  const response = await axiosInstance.get("/recipes");
+export const getRecipes = async (
+  filters: ActiveFilters = {}
+): Promise<Recipe[]> => {
+  const params = {
+    params: {
+      ...filters,
+      _page: filters.page || 1,
+      _expand: ["difficulty", "cuisine", "diet"],
+      _limit: 8,
+    },
+  };
+  const response = await axiosInstance.get("/recipes", params);
+  if (!response.data) {
+    return [];
+  }
+
+  const data: Recipe[] = response.data.map((item: Recipe) => {
+    return {
+      ...item,
+      rating: "5",
+      image: resolvePath(item.image),
+    };
+  });
+
+  return data;
+};
+
+export const getCategory = async (type: string): Promise<Category[]> => {
+  const response = await axiosInstance.get(`/${type}`);
   return response.data;
 };
--- client/src/pages/recipes.tsx
+++ client/src/pages/recipes.tsx
@@ -5,50 +5,84 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import SearchBar from "../components/search-bar";
 import { toast } from "react-hot-toast";
-import { getRandomRating, resolvePath } from "../lib/utils";
 import { getRecipes } from "../api/recipes";
+import { useState } from "react";
+import { LoaderCircle } from "lucide-react";
 
 function Recipes() {
-  const { data, isError, error } = useQuery<Recipe[], Error>({
-    queryKey: ["recipes"],
-    queryFn: getRecipes,
+  //State for tracking all filters
+  const [activeFilters, setActiveFilters] = useState<ActiveFilters>({
+    page: "1",
+  });
+
+  //Fetch data
+  const { data, isLoading, isError, error } = useQuery<Recipe[], Error>({
+    queryKey: ["recipes", activeFilters],
+    queryFn: () => getRecipes(activeFilters),
   });
 
   if (isError) {
-    toast.error(`Si è verificato un errore: ${error?.message}`);
+    toast.error(`Something went wrong: ${error?.message}`);
   }
+
+  //Handle filters
+  const toggleFilter = (key: FilterKey, value?: string) => {
+    setActiveFilters((prevFilters) => {
+      const newFilters = { ...prevFilters };
+      if (!value) {
+        delete newFilters[key];
+      } else {
+        newFilters[key] = value;
+      }
+      return { ...newFilters, page: 1 };
+    });
+  };
+
+  //Search recipe
+  const onSearch = (text: string) => {
+    toggleFilter("q", text.length > 0 ? text : undefined);
+  };
 
   return (
     <main className="min-h-screen">
       <Header />
-      <div className="px-20 py-20 flex flex-col gap-y-5">
-        <SearchBar />
-        <div className="flex justify-between w-full items-center">
-          <Filters
-            filters={["Vegetarian", "Gluten-free", "Mexican", "Healthy"]}
-            activeFilters={["Mexican"]}
-          />
+      <div className="px-20 py-10 flex flex-col gap-y-5">
+        <SearchBar onSearch={onSearch} value={activeFilters.q || ""} />
+        <div className="flex justify-between w-full items-start">
+          <Filters toggleFilters={toggleFilter} activeFilters={activeFilters} />
           <button className="bg-btn-primary hover:bg-btn-primary-hovered px-6 py-1.5 rounded-md text-white font-medium">
             Upload your recipe
           </button>
         </div>
 
-        <div className="flex-1 flex flex-wrap justify-center gap-10 pt-4">
-          {data ? (
+        <div className="flex-1 flex flex-wrap justify-center gap-y-10 gap-x-8 pt-4 ">
+          {isLoading ? (
+            <div className="min-h-[50vh] flex-1 flex items-center justify-center">
+              <LoaderCircle
+                size={40}
+                strokeWidth={3}
+                className="animate-spin text-zinc-500"
+              />
+            </div>
+          ) : data && data.length > 0 ? (
             data.map((recipe) => (
               <CardRecipe
                 key={recipe.id}
-                imageSrc={resolvePath(recipe.image)}
+                imageSrc={recipe.image}
                 title={recipe.name}
                 description={recipe.instructions}
-                difficulty={recipe.difficultyId}
-                diet={recipe.dietId}
-                cuisine={recipe.cuisineId}
-                rating={getRandomRating()}
+                difficulty={recipe.difficulty.name}
+                diet={recipe.diet.name}
+                cuisine={recipe.cuisine.name}
+                rating={recipe.rating}
               />
             ))
           ) : (
-            <div></div>
+            <div className="min-h-[50vh] flex-1 flex items-center justify-center">
+              <div className="text-zinc-400 text-xl font-medium">
+                No recipe found
+              </div>
+            </div>
           )}
         </div>
       </div>
