a475a067f6c42de511d7848c0787601e1f58c2ab
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-02 08:50:32 +0200

    feat: install react-router-dom and set routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
-//import LandingPage from "./pages/landing-page";
+import LandingPage from "./pages/landing-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Recipes from "./pages/recipes";
 import { Toaster } from "react-hot-toast";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/recipes",
+    element: <Recipes />,
+  },
+]);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Recipes />
+      <RouterProvider router={router} />
       <Toaster />
     </QueryClientProvider>
   );
-  // return <LandingPage />;
 }
 
 export default App;
