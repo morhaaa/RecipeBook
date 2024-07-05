a7ba22b3c2b1ab89ad36cdf328b0541d2c9b43ef
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-07-05 21:33:55 +0200

    refactor(recipe): improve code readability and maintainability

--- client/src/pages/recipe-page.tsx
+++ client/src/pages/recipe-page.tsx
@@ -14,18 +14,45 @@ import {
 } from "lucide-react";
 import toast from "react-hot-toast";
 import Comments from "../components/comments";
+import { getComments } from "../api/comments";
 
 function RecipePage() {
   const { recipeId } = useParams();
 
-  // Fetch data
-  const { data, isLoading, isError, error } = useQuery<Recipe, Error>({
+  const {
+    data: recipeData,
+    isLoading: isLoadingRecipe,
+    isError: isErrorRecipe,
+    error: errorRecipe,
+  } = useQuery<Recipe, Error>({
     queryKey: [`recipe_${recipeId}`],
     queryFn: () => getRecipeInfo(recipeId || ""),
+    enabled: !!recipeId,
   });
 
-  if (isError) {
-    toast.error(`Something went wrong: ${error?.message}`);
+  // Fetch comments data
+  const {
+    data: commentsData,
+    isLoading: isLoadingComments,
+    isError: isErrorComments,
+    error: errorComments,
+  } = useQuery<CommentRecipe[], Error>({
+    queryKey: [`comments_${recipeId}`],
+    queryFn: () => getComments(recipeId || ""),
+    enabled: !!recipeId,
+  });
+
+  //Errors
+  if (isErrorRecipe) {
+    toast.error(
+      `Something went wrong with the recipe: ${errorRecipe?.message}`
+    );
+  }
+
+  if (isErrorComments) {
+    toast.error(
+      `Something went wrong with the comments: ${errorComments?.message}`
+    );
   }
 
   return (
@@ -33,7 +60,7 @@ function RecipePage() {
       <Header />
       <ScrollRestoration />
 
-      {isLoading ? (
+      {isLoadingRecipe ? (
         <div className=" flex-1 flex items-center justify-center  min-h-[80vh] text-zinc-500">
           <LoaderCircle
             size={40}
@@ -41,7 +68,7 @@ function RecipePage() {
             className="animate-spin text-zinc-500"
           />
         </div>
-      ) : data ? (
+      ) : recipeData ? (
         <div className="lg:px-20 px-10 md:px-14 pb-10 pt-20 flex flex-col gap-y-4">
           <Link to="/recipes">
             <button className="font-bold text-lg text-primary-orange flex items-center gap-x-0.5 hover:underline">
@@ -50,27 +77,31 @@ function RecipePage() {
           </Link>
           <div className="flex flex-col md:flex-row gap-y-4 gap-x-10 flex-1">
             <img
-              src={resolvePath(data.image)}
-              alt={data.name}
+              src={resolvePath(recipeData.image)}
+              alt={recipeData.name}
               className=" w-full max-h-80 md:w-80 md:h-80 lg:w-96 lg:h-96 object-cover rounded-xl"
             />
             <div className="flex-1 flex flex-col gap-6">
               {/* Header */}
               <div className="flex flex-col border-b py-2">
-                <h1 className="text-4xl font-bold mb-2">{data.name}</h1>
+                <h1 className="text-4xl font-bold mb-2">{recipeData.name}</h1>
                 <div className="flex gap-x-4">
                   <div className="flex justify-center items-center gap-x-1.5">
                     <CookingPot size={16} strokeWidth={1.25} />
-                    <span className="text-secondary">{data.cuisine.name}</span>
+                    <span className="text-secondary">
+                      {recipeData.cuisine.name}
+                    </span>
                   </div>
                   <div className="flex justify-center items-center gap-x-1.5">
                     <ChefHat size={16} strokeWidth={1.25} />
-                    <span className="text-secondary">{data.diet.name}</span>
+                    <span className="text-secondary">
+                      {recipeData.diet.name}
+                    </span>
                   </div>
                   <div className="flex justify-center items-center gap-x-1.5">
                     <BarChart size={20} strokeWidth={1.25} />
                     <span className="text-secondary">
-                      {data.difficulty.name}
+                      {recipeData.difficulty.name}
                     </span>
                   </div>
                   <p className="text-primary-orange text-lg font-medium">5 ★</p>
@@ -81,7 +112,7 @@ function RecipePage() {
                 <div>
                   <h2 className="text-2xl font-semibold mb-2">Ingredients</h2>
                   <ul className="list-disc list-inside">
-                    {data.ingredients.map((ingredient, index) => (
+                    {recipeData.ingredients.map((ingredient, index) => (
                       <li key={index} className="text-secondary">
                         {ingredient}
                       </li>
@@ -91,13 +122,23 @@ function RecipePage() {
                 <div>
                   <h2 className="text-2xl font-semibold mb-2">Instructions</h2>
                   <p className="text-gray-700 whitespace-pre-line">
-                    {data.instructions}
+                    {recipeData.instructions}
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <Comments recipeId={recipeId || ""} />
+          {isLoadingComments ? (
+            <div className=" flex-1 flex items-center justify-center  min-h-[20vh] pt-20 text-zinc-500">
+              <LoaderCircle
+                size={40}
+                strokeWidth={3}
+                className="animate-spin text-zinc-500"
+              />
+            </div>
+          ) : (
+            <Comments comments={commentsData || []} />
+          )}
         </div>
       ) : (
         <div className="flex-1 flex flex-col items-center justify-center min-h-[80vh] text-zinc-500">
